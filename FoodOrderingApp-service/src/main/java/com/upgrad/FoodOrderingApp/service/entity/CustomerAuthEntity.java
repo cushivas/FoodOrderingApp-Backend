dc0e33d98c64deb495d0ca2a34827fd3bd8df6b7
Swapnadeep Dutta <53987149+swapdutt@users.noreply.github.com>
2021-05-30 22:39:31 +0530

    swapnadeep.dutta : Developed the entity classes for Customer

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerAuthEntity.java
@@ -0,0 +1,150 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+/**
+ * @author swapnadeep.dutta
+ * 
+ * This class is for fetching and storing the information regarding the customer authentication
+ */
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "customer_auth")
+@NamedQueries({
+		@NamedQuery(name = "customerAuthTokenByAccessToken", query = "select ct from CustomerAuthEntity where ct.accessToken = :accessToken") })
+public class CustomerAuthEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "uuid")
+	@Size(max = 200)
+	@NonNull
+	private String uuid;
+
+	@ManyToOne
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JoinColumn(name = "customer_id")
+	private CustomerEntity customer;
+
+	@Column(name = "access_token")
+	@Size(max = 500)
+	@NotNull
+	private String accessToken;
+
+	@Column(name = "login_at")
+	@NotNull
+	private ZonedDateTime loginAt;
+
+	@Column(name = "login_out")
+	@NotNull
+	private ZonedDateTime loginOut;
+
+	@Column(name = "expires_at")
+	@NotNull
+	private ZonedDateTime expiresAt;
+
+	// No-argument Constructor for empty initializing
+
+	public CustomerAuthEntity() {
+
+	}
+
+	// Initialized constructor
+
+	public CustomerAuthEntity(long id, @Size(max = 200) String uuid, CustomerEntity customer,
+			@Size(max = 500) @NotNull String accessToken, @NotNull ZonedDateTime loginAt,
+			@NotNull ZonedDateTime loginOut, @NotNull ZonedDateTime expiresAt) {
+		this.id = id;
+		this.uuid = uuid;
+		this.customer = customer;
+		this.accessToken = accessToken;
+		this.loginAt = loginAt;
+		this.loginOut = loginOut;
+		this.expiresAt = expiresAt;
+	}
+
+	/*
+	 * Getters and Setters method to fetch and store the information in the
+	 * databases
+	 */
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public CustomerEntity getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(CustomerEntity customer) {
+		this.customer = customer;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public ZonedDateTime getLoginAt() {
+		return loginAt;
+	}
+
+	public void setLoginAt(ZonedDateTime loginAt) {
+		this.loginAt = loginAt;
+	}
+
+	public ZonedDateTime getLoginOut() {
+		return loginOut;
+	}
+
+	public void setLoginOut(ZonedDateTime loginOut) {
+		this.loginOut = loginOut;
+	}
+
+	public ZonedDateTime getExpiresAt() {
+		return expiresAt;
+	}
+
+	public void setExpiresAt(ZonedDateTime expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+
+}
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
@@ -1,5 +1,184 @@
 package com.upgrad.FoodOrderingApp.service.entity;
 
-public class CustomerEntity {
+/**
+ * @author swapnadeep.dutta
+ * 
+ * This class is for fetching and storing the information regarding the customer
+ */
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@Entity
+@Table(name = "customer")
+@NamedQueries({
+		@NamedQuery(name = "customerByContactNumber", query = "select c from CustomerEntity c where c.contactNumber = :contactNumber") })
+public class CustomerEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "uuid")
+	@Size(max = 200)
+	@NotNull
+	private String uuid;
+
+	@Column(name = "firstname")
+	@Size(max = 30)
+	@NotNull
+	private String firstName;
+
+	@Column(name = "lastname")
+	@Size(max = 30)
+	@NotNull
+	private String lastName;
+
+	@Column(name = "email")
+	@Size(max = 50)
+	@NotNull
+	@Email(message = "Please enter a valid email address")
+	private String email;
+
+	@Column(name = "contact_number")
+	@Size(max = 30)
+	@NotNull
+	private String contactNumber;
+
+	@Column(name = "password")
+	@Size(max = 255)
+	@NotNull
+	private String password;
+
+	@Column(name = "salt")
+	@Size(max = 255)
+	@NotNull
+	private String salt;
+
+	// No-argument Constructor for empty initializing
+
+	public CustomerEntity() {
+
+	}
+
+	// Initialized constructor
+
+	public CustomerEntity(long id, @Size(max = 200) @NotNull String uuid, @Size(max = 30) @NotNull String firstName,
+			@Size(max = 30) @NotNull String lastName,
+			@Size(max = 50) @NotNull @Email(message = "Please enter a valid email address") String email,
+			@Size(max = 30) @NotNull String contactNumber, @Size(max = 255) @NotNull String password,
+			@Size(max = 255) @NotNull String salt) {
+		this.id = id;
+		this.uuid = uuid;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.contactNumber = contactNumber;
+		this.password = password;
+		this.salt = salt;
+	}
+
+	/*
+	 * Getters and Setters method to fetch and store the information in the
+	 * databases
+	 */
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return new EqualsBuilder().append(this, obj).isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(this).hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
 
 }
