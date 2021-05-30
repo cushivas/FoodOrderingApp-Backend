cca232af9b312ed98eaeff96d81b6b9598c4b0d4
Swapnadeep Dutta <53987149+swapdutt@users.noreply.github.com>
2021-05-30 22:41:31 +0530

    swapnadeep.dutta : Developed the dao class for Customer

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
@@ -1,5 +1,51 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.upgrad.FoodOrderingApp.service.entity.CustomerAuthEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+
+@Repository
 public class CustomerDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public CustomerEntity createCustomer(CustomerEntity customerEntity) {
+		this.entityManager.persist(customerEntity);
+		return customerEntity;
+	}
+
+	public CustomerEntity getCustomerByContactNumber(String contactNumber) {
+		try {
+			return (CustomerEntity) this.entityManager.createNamedQuery("customerByContactNumber", CustomerEntity.class)
+					.setParameter("contactNumber", contactNumber).getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public CustomerAuthEntity createAuthenticationToken(CustomerAuthEntity customerAuthEntity) {
+		this.entityManager.persist(customerAuthEntity);
+		return customerAuthEntity;
+	}
+
+	public CustomerAuthEntity getCustomerAuthToken(String accessToken) {
+		try {
+			return entityManager.createNamedQuery("customerAuthTokenByAccessToken", CustomerAuthEntity.class)
+					.setParameter("accessToken", accessToken).getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public void updateCustomer(CustomerEntity customerEntity) {
+		this.entityManager.merge(customerEntity);
+
+	}
 
 }
